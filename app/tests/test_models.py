660bed27d12348d8ae84ec573f86ad7937277e4d
Unknown <ali1391ali1391@yahoo.com>
2019-07-24 17:42:01 +0430

    until normalization email address

---
+++ app/tests/test_models.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """
+        test use create successful
+
+        :return:
+        """
+
+        email = 'test@londonappdev.com'
+        password = 'Password123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """normalized email address"""
+        email = "ali@GMAIL.COM"
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
